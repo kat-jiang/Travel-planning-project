4ab3a63d4f713dd5a6c0a6cf80101d8420867a04
Katherine Jiang <katherine.jiang30@gmail.com>
2022-07-06 19:55:55 +0000

    updated files for heroku deployment

--- server.py
+++ server.py
@@ -773,5 +773,4 @@ def get_activity_locations():
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
     connect_to_db(app)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
